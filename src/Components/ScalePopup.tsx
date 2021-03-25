ff3195913fae5807950785d204160b28f9da997d
Евгений Старцев <startsevjm@gmail.com>
2021-03-25 23:33:13 +0800

    boiuh

---
+++ src/Components/ScalePopup.tsx
@@ -0,0 +1,24 @@
+import React, {ReactElement} from 'react';
+
+const ScalePopup = (): ReactElement => {
+    return (
+        <div>
+            <div>
+                <div>Обратный расчет</div>
+                <div>кнопка</div>
+                <div>переключатель вертик</div>
+                <div>селектор</div>
+                <div>редактирование</div>
+            </div>
+            <div>оранжевый блок с процентами</div>
+            <div>табличная часть
+                <div> k рук</div>
+                <div> k</div>
+                <div></div>
+            </div>
+        </div>
+    )
+}
+
+
+export default ScalePopup;
